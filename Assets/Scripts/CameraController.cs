af9652157e251e6c30ffd33aaedddf54f916a783
ina-amagami <ina.amagami@gmail.com>
2020-12-04 15:57:44 +0900

    Initial Commit.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	private Transform followTarget = null;
+
+	private void Start()
+	{
+		followTarget = GameObject.FindGameObjectWithTag(Player.Tag).transform;
+	}
+
+	private void Update()
+	{
+		var pos = transform.position;
+		pos.x = followTarget.position.x;
+		transform.position = pos;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public const string Tag = "Player";
+
+    [Header("スクロール速度")]
+    [SerializeField] private float moveSpeed = 1f;
+
+    private Material materialInstance = null;
+
+    private void Start()
+    {
+        materialInstance = GetComponent<MeshRenderer>().material;
+    }
+
+    private void Update()
+    {
+        // 自動で右方向に移動
+        transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        materialInstance.color = Color.red;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        materialInstance.color = Color.blue;
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(materialInstance);
+    }
+}
